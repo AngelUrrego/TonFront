2522320e7a8a6d2a7984ad2b9ce0c2c3696b1460
AngelUrrego <angelurrego2010@gmail.com>
2024-12-13 19:19:52 -0500

    Front-end

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            // Polyfill para Buffer
-            buffer: 'buffer',
-        },
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            // Polyfill para global y process
-            define: {
-                global: 'globalThis',
-            },
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                }),
-            ],
-        },
-    },
+  plugins: [react(), nodePolyfills()],
+  base: '/',
 });
